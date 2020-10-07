1da4852935a5fcac26720d8ae573f8fede0b6b6a
lorenzo-damico <loredam@hotmail.it>
2020-10-07 17:42:28 +0200

    Aggiunta immagine album

--- index.php
+++ index.php
@@ -6,6 +6,12 @@ include "partials/head.php";
 
 
   <body>
-   
+    <div class="container">
+      <?php foreach ($database as $album) { ?>
+        <div class="album">
+          <img src="<?php echo $album["poster"]; ?>" alt="copertina album">
+        </div>
+      <?php } ?>
+    </div>
   </body>
 </html>
