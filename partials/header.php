ccb96a5c071c89d8067be96c74be7af14dd7694b
lorenzo-damico <loredam@hotmail.it>
2020-10-07 19:10:32 +0200

    Completata parte grafica e parzializzazione file php

---
+++ partials/header.php
@@ -0,0 +1,10 @@
+<body>
+  <header>
+    <div class="container">
+      <div class="header-section">
+        <div class="logo">
+          <img src="img/logo-spotify.png" alt="logo spotify">
+        </div>
+      </div>
+    </div>
+  </header>
